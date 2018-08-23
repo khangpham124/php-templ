60a1aedfff7ef911b409f28e9f63796f1f6ff567
khangpham <khangpham421@gmail.com>
2018-08-23 16:40:27 +0700

    fix app config

--- app_config.php
+++ app_config.php
@@ -2,19 +2,6 @@
 	define("APP_URL", "http://teddycoder.com/projects/navicontrol/");
 	define("APP_PATH", "/home/teddycoder/public_html/projects/navicontrol/");
 	define("APP_PATH", "/home/teddycoder/public_html/projects/navicontrol/");
-	
-	function getArrUrl($var)
-	  {
-			$nvar = Array();
-			$na = explode("/", $var);
-			for($i=0; $i<count($na)-1;$i+=4)
-			{
-				$nvar["$na[$i]"] = $na[$i+1];
-			}
-			return $nvar;
-	  }
-	
-	  $args = getArrUrl($_GET['args']);
 ?>
 
 
